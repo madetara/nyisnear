e4ceba9f1401ef953ac05637ced9ba702a3c5754
dv.kab <dv.kab@skbkontur.ru>
2021-11-30 23:48:17 +0300

    feat: cover more pronunciations

--- src/core/decider.rs
+++ src/core/decider.rs
@@ -8,7 +8,7 @@ use std::sync::atomic::{AtomicU8, Ordering};
 pub async fn should_respond(msg: &str) -> bool {
     lazy_static! {
         static ref NY_REGEX: Regex = Regex::new(
-            r"((\b|^|\W)[Нн][Гг](\b|$|\W))|((\b|^|\W)[Хх][Аа][Тт][Аа](\b|$|\W))|((\b|^|\W)[Нн]ов(ый|ым|ому) [Гг]од(ом|у)?(\b|$|\W))"
+            r"((\b|^|\W)[Нн][Гг](\b|$|\W))|((\b|^|\W)[Хх][Аа]+[Тт][АаУуЫыЕе]+(\b|$|\W))|((\b|^|\W)[Нн]ов(ый|ым|ому) [Гг]од(ом|у)?(\b|$|\W))"
         )
         .unwrap();
 
